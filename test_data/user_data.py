451d31294f476923555b5f827b29e82e6cc76ca7
Mikiyas Alehegn <mikiyasalehegn@Mikiyass-MacBook-Pro.local>
2026-03-30 19:50:28 +0300

    creating fixture for tempo repo

--- test_data/user_data.py
+++ test_data/user_data.py
@@ -1,11 +1,9 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
+from utils.config import USERNAME, TOKEN, BASE_URL
 
 class UserTestData:
-    user_name = os.getenv("USERNAME")
+    user_name = USERNAME
+    token = TOKEN
+    base_url = BASE_URL
     new_user_name = "New User"
     user_type = "User"
     twitter_username = "Twitter Username"
