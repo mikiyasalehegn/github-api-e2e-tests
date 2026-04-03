65579c0f8c95dbd7000779c6cd7381bbbb47eec9
Mikiyas Alehegn <mikiyasalehegn@Mikiyass-MacBook-Pro.local>
2026-04-03 20:57:04 +0300

    delete branch protection

--- tests/e2e/test_branch_protection_lifecycle.py
+++ tests/e2e/test_branch_protection_lifecycle.py
@@ -46,4 +46,24 @@ class TestBranchProtectionLifecycle(BaseTest):
         assert assert_branch_protection.allow_deletions["enabled"] == True
 
 
+        # -------------------- Check Branch Protection --------------------
+        time.sleep(1)
+        protection_resp = self.branch_api.get_branch_protection(owner=USERNAME, repo=branch_temp_repo)
+        logger.info(f"protection_resp data: {protection_resp.text}")
+        assert protection_resp.status_code == 200
+        assert protection_resp.json()["required_pull_request_reviews"]["required_approving_review_count"] == 1
+
+
+        # -------------------- Delete Branch Protection --------------------
+        time.sleep(1)
+        delete_branch_protection = self.branch_api.delete_branch_protection(owner=USERNAME, repo=branch_temp_repo)
+        logger.info(f"delete_branch_protection data: {delete_branch_protection.text}")
+        assert delete_branch_protection.status_code == 204
+
+
+        # -------------------- Get Branch Protection --------------------
+        get_branch_protection_resp = self.branch_api.get_branch_protection(owner=USERNAME, repo=branch_temp_repo)
+        logger.info(f"get_branch_protection_resp data: {get_branch_protection_resp.text}")
+        assert get_branch_protection_resp.status_code == 404
+        assert get_branch_protection_resp.json()["message"] == "Branch not protected"
 
