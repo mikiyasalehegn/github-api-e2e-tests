dedf5c51e65f3995441c0354b0d83048817fba9d
Mikiyas Alehegn <mikiyasalehegn@Mikiyass-MacBook-Pro.local>
2026-04-08 21:07:36 +0300

    test create repo with unauthorized token

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
-from .config import BASE_URL, TOKEN, USERNAME, COLLABORATOR, COLLABORATOR_TOKEN
+from .config import BASE_URL, TOKEN, USERNAME, COLLABORATOR, COLLABORATOR_TOKEN, READ_ONLY_TOKEN
 from .assertion import *
 from .decorators import slow_down
