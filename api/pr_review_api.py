8a6c8bb334c14476b793fa73220feb01195abe63
Mikiyas Alehegn <mikiyasalehegn@Mikiyass-MacBook-Pro.local>
2026-04-15 20:12:20 +0300

    request pr review

--- api/pr_review_api.py
+++ api/pr_review_api.py
@@ -5,8 +5,8 @@ class PullRequestReviewAPI:
     def get_pr_review_request(self, owner, repo, pr_number):
         return self.client.get(f"/repos/{owner}/{repo}/pulls/{pr_number}/requested_reviewers")
 
-    def request_pr_review(self, owner, repo, pr_number):
-        return self.client.post(f"/repos/{owner}/{repo}/pulls/{pr_number}/requested_reviewers")
+    def request_pr_review(self, owner, repo, pr_number, data):
+        return self.client.post(f"/repos/{owner}/{repo}/pulls/{pr_number}/requested_reviewers", payload=data)
 
     def remove_pr_review(self, owner, repo, pr_number):
         return self.client.delete(f"/repos/{owner}/{repo}/pulls/{pr_number}/requested_reviewers")
--- tests/e2e/test_pr_review_assignment_full_flow.py
+++ tests/e2e/test_pr_review_assignment_full_flow.py
@@ -1,8 +1,9 @@
 import pytest
 from base import BaseTest
-from api import PullRequestReviewAPI
+from api import PullRequestReviewAPI, RepoCollabApi, InvitationApi
 from test_data import UserTestData
 import logging
+from utils import USERNAME, COLLABORATOR, COLLABORATOR_TOKEN
 
 
 logger = logging.getLogger(__name__)
@@ -16,6 +17,7 @@ class TestPRReviewAssignmentFullFlow(BaseTest):
 
         client = self.get_client()
         self.pr_review_api = PullRequestReviewAPI(client)
+        self.repo_collab_api = RepoCollabApi(client)
 
     @pytest.mark.parametrize("number_of_branches", [1], indirect=True)
     def test_pr_review_assignment_full_flow(self, number_of_branches, temporary_branches_with_prs, create_temporary_repo):
@@ -23,17 +25,30 @@ class TestPRReviewAssignmentFullFlow(BaseTest):
         repo_name = create_temporary_repo
 
         # ------------------- get pr review -------------------
-        get_pr_review_response = self.pr_review_api.get_pr_review_request(owner=UserTestData.user_name, repo=repo_name,
-                                                                          pr_number=pr_numbers[0])
-        logger.info(f"get_pr_review_response: {get_pr_review_response.text}")
-        get_pr_review_response.status_code = 200
+        # get_pr_review_response = self.pr_review_api.get_pr_review_request(owner=UserTestData.user_name, repo=repo_name,
+        #                                                                   pr_number=pr_numbers[0])
+        # logger.info(f"get_pr_review_response: {get_pr_review_response.text}")
+        # get_pr_review_response.status_code = 200
 
+        # ------------------- add collaborator -------------------
+        add_repo_collab_response = self.repo_collab_api.add_repo_collaborator(owner=USERNAME, repo=repo_name,
+                                                                              username=COLLABORATOR)
+        logger.info(f"add_repo_collaborator response: {add_repo_collab_response.text}")
+        assert add_repo_collab_response.status_code == 201
+        invitation_id = add_repo_collab_response.json()["id"]
+
+        # ------------------- accept repo collab invitation -------------------
+        invitation_client = self.get_client(token=COLLABORATOR_TOKEN)
+        accept_invitation_api = InvitationApi(invitation_client)
+        accept_invitation_response = accept_invitation_api.accept_invitation(invitation_id=invitation_id)
+        logger.info(f"accept_invitation response: {accept_invitation_response.text}")
 
         # ------------------- request pr review -------------------
-        # request_review_response = self.pr_review_api.get_pr_review_request(owner=UserTestData.user_name, repo=repo_name,
-        #                                                                    pr_number=pr_numbers[0])
-        # logger.info(f"request_review_response: {request_review_response.text}")
-        # assert get_pr_review_response.status_code == 200
+        request_review_response = self.pr_review_api.request_pr_review(owner=UserTestData.user_name, repo=repo_name,
+                                                                       pr_number=pr_numbers[0],
+                                                                       data={"reviewers": [COLLABORATOR]})
+        logger.info(f"request_review_response: {request_review_response.text}")
+        assert request_review_response.status_code == 201
 
 
 
